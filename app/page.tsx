6b4e1da64e96fad6b0b7808078f0a5423beb6fac
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2024-03-16 00:28:40 +0300

    Font and color

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-mycolor-700 ">
+      <h1 className="font-pacifico">test</h1>
+      <h1 className="font-poppins">test</h1>
+
+      <h1 className="font-roboto">test</h1>
+
+      <h1 className="font-mochiy">test</h1>
+      <h1>test</h1>
+
 
      
     </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,26 @@ const config = {
       },
     },
     extend: {
+      fontFamily:{
+        poppins:["Poppins", "sans-serif"],
+        mochiy: ["Mochiy Pop One", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+        pacifico: ["Pacifico", "cursive"],
+
+      },
+      colors:{
+        mycolor:{
+          100: "#5FB0CE",
+          200: "#1F466F",
+          300: "#364149",
+          400: "#0D0D11",
+          500: "#3E4548",
+          600: "#323B44",
+          700: "#F4F4F4"
+
+
+        }
+      },
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
