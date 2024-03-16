3e2d288168dcf2b16e3a047c37a0e9ff328aa4eb
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2024-03-17 00:53:59 +0300

    Footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import About from "@/components/About/About";
 import Archive from "@/components/Arcihive/Archive";
 import Contact from "@/components/Contact/Contact";
+import Footer from "@/components/Footer/Footer";
 import Hero from "@/components/Hero/Hero";
 import Navbar from "@/components/Navbar/Navbar";
 import Project from "@/components/Projects/Project";
@@ -22,6 +23,7 @@ export default function Home() {
 
 
     </div>
+    <Footer/>
 
 
 
