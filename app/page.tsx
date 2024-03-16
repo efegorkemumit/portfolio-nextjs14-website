821d024dd7ae809c8d23b0928e65c13266324f67
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2024-03-17 00:48:57 +0300

    Contact Form

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import About from "@/components/About/About";
 import Archive from "@/components/Arcihive/Archive";
+import Contact from "@/components/Contact/Contact";
 import Hero from "@/components/Hero/Hero";
 import Navbar from "@/components/Navbar/Navbar";
 import Project from "@/components/Projects/Project";
@@ -17,6 +18,7 @@ export default function Home() {
       <Archive/>
       <About/>
       <Project/>
+      <Contact/>
 
 
     </div>
